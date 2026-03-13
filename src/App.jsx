0d06265f0d77f0ca0b0081574abb51b1180b9f76
Satria Kamal Norsaid <satriakamaln@gmail.com>
2026-03-13 17:33:39 +0800

    sort

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ export default function App() {
                 onToggleItem={handleToggleItem}
                 onClearItems={handleClearItems}
             />
-            <Footer />
+            <Footer items={items} />
         </div>
     );
 }
@@ -199,10 +199,22 @@ function Item({ item, onDeleteItem, onToggleItem }) {
     );
 }
 
-function Footer() {
+function Footer({ items }) {
+    if (items.length === 0)
+        return (
+            <>
+                <footer className="stats">Daftar belanja masih kosong</footer>;
+            </>
+        );
+
+    const totalItems = items.length;
+    const checkedItems = items.filter((item) => item.checked).length;
+    const percentage = Math.round((checkedItems / totalItems) * 100);
+
     return (
         <footer className="stats">
-            Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)
+            Ada {totalItems} barang di daftar belanjaan, {checkedItems} barang
+            sudah dibeli ({percentage}%)
         </footer>
     );
 }
